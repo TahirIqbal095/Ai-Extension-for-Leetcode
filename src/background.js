133c85107a6e98c77979fe04ee4ea08393258cfa
Tahir Iqbal <shahtahir786@gmail.com>
2025-04-07 14:30:18 +0530

    fix: service worker

--- src/background.js
+++ src/background.js
@@ -1,1 +1,10 @@
-console.log("Background script running...");
+// Make sure this file uses ES modules syntax if "type": "module" is specified in manifest
+console.log("Background script loaded");
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "openPopup") {
+        chrome.action.openPopup(); // Opens the popup programmatically
+    }
+    // Always return true for async operations
+    return true;
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import manifest from "./manifest.json";
 import path from "path";
 import { crx } from "@crxjs/vite-plugin";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import manifest from "./public/manifest.json";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        crx({ manifest }),
-        viteStaticCopy({
-            targets: [
-                // Optionally, copy the entire src folder
-                // { src: "src/*", dest: "src" },
-                // Copy the background.js file to the dist/src folder
-                { src: "src/background.js", dest: "src" },
-                // Copy the manifest.json file to the root of the dist folder
-                { src: "manifest.json", dest: "" },
-            ],
-        }),
-    ],
-
+    plugins: [react(), tailwindcss(), crx({ manifest })],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
