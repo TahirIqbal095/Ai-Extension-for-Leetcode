bbe5944874d04036480f05fe937064d662601a2f
Tahir Iqbal <shahtahir786@gmail.com>
2025-04-15 13:01:21 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,31 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { crx } from "@crxjs/vite-plugin";
-import manifest from "./public/manifest.json";
+import manifest from "./manifest.config";
 import tailwindcss from "@tailwindcss/vite";
+import hotReloadExtension from "hot-reload-extension-vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), crx({ manifest })],
+    plugins: [
+        react(),
+        tailwindcss(),
+        crx({
+            manifest,
+            contentScripts: {
+                preambleCode: false,
+            },
+        }),
+        hotReloadExtension({
+            log: true,
+            backgroundPath: "./src/background.js",
+        }),
+    ],
+
+    build: {
+        outDir: "dist",
+        emptyOutDir: false,
+    },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
