4ff206e158ab4fdc461d897d144042a877ded121
Tahir Iqbal <shahtahir786@gmail.com>
2025-04-18 14:27:30 +0530

    updated output schema

--- src/schema/outputMode.ts
+++ src/schema/outputMode.ts
@@ -1,34 +1,11 @@
 import { z } from "zod";
 
-const SupportedLanguages = [
-    "c",
-    "cpp",
-    "csharp",
-    "cs",
-    "dart",
-    "elixir",
-    "erlang",
-    "go",
-    "java",
-    "javascript",
-    "jsonp",
-    "jsx",
-    "php",
-    "python",
-    "racket",
-    "rkt",
-    "ruby",
-    "rb",
-    "rust",
-    "scala",
-    "sql",
-    "Swift",
-    "typescript",
-    "tsx",
-] as const;
-
 export const outputSchema = z.object({
-    feedback: z.string(),
+    feedback: z
+        .string()
+        .describe(
+            "short, personalized analysis of the user's code (if provided) highlighting mistakes, inefficiencies, or areas of improvement. Avoid generic explanations or restating the problem."
+        ),
     hints: z
         .array(z.string())
         .max(2, "You can only provide up to 2 hints.")
@@ -38,8 +15,4 @@ export const outputSchema = z.object({
         .string()
         .optional()
         .describe("code snippet should be in format."),
-    programmingLanguage: z
-        .enum(SupportedLanguages)
-        .optional()
-        .describe("Programming language code as supports by prismjs"),
 });
