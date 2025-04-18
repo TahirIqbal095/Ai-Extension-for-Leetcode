f0583129a8b041ba2288535d45112f7eec72fb60
Tahir Iqbal <shahtahir786@gmail.com>
2025-04-18 18:02:01 +0530

    model service

---
+++ src/services/ModalService.ts
@@ -0,0 +1,34 @@
+import { ValidModel } from "@/constants/valid_models";
+import {
+    GenerateResponseParamsType,
+    ModelInterface,
+} from "@/interface/ModalInterface";
+import { models } from "@/modals";
+import { outputSchema } from "@/schema/outputMode";
+import { z } from "zod";
+
+export class ModelService {
+    private model: ModelInterface | null = null;
+
+    selectModel(apiKey: string, modelName: ValidModel) {
+        if (models[modelName]) {
+            this.model = models[modelName];
+            this.model.init(apiKey);
+        } else {
+            throw new Error(`Model ${modelName} not found`);
+        }
+    }
+
+    async generate(props: GenerateResponseParamsType): Promise<
+        Promise<{
+            success: z.infer<typeof outputSchema> | null;
+            error: unknown;
+        }>
+    > {
+        if (!this.model) {
+            throw new Error(`No model selected`);
+        }
+
+        return this.model.generateResponse(props);
+    }
+}
