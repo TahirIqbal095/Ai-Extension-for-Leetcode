966fb2c07de5a09b5bcaed514cf8db701f65cccc
Tahir Iqbal <shahtahir786@gmail.com>
2025-05-06 22:38:13 +0530

    chat history fix

--- src/content/ChatWindow.tsx
+++ src/content/ChatWindow.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, forwardRef } from "react";
 import { Textarea } from "@/components/ui/textarea/Textarea";
 import { ModelService } from "@/services/ModalService";
 import { ChatHistory } from "@/interface/chatHistory";
@@ -9,7 +9,7 @@ import {
     AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Maximize2, Minimize2, SendHorizontal } from "lucide-react";
-import { addChatHistory, getChatHistory, CHAT_HISTORY_KEY } from "@/lib/indexedDb";
+import { getChatHistory, CHAT_HISTORY_KEY, addChatHistory } from "@/lib/indexedDb";
 
 const GEMINI_API = import.meta.env.VITE_GOOGLE_GEMINI_KEY;
 
@@ -20,163 +20,170 @@ type ChatWindowProps = {
     setPrompt: (prompt: string) => void;
 };
 
-export function ChatWindow({ setPrompt, prompt, systemPrompt, code }: ChatWindowProps) {
-    const [maximize, setMaximize] = useState<boolean>(false);
-    const [value, setValue] = useState<string>("");
-    const [chatHistory, setChatHistory] = useState<ChatHistory[]>([]);
-    const messageEndRef = useRef<HTMLDivElement>(null);
+export const ChatWindow = forwardRef<HTMLTextAreaElement, ChatWindowProps>(
+    ({ setPrompt, prompt, systemPrompt, code }, ref) => {
+        const [maximize, setMaximize] = useState<boolean>(false);
+        const [value, setValue] = useState<string>("");
+        const [chatHistory, setChatHistory] = useState<ChatHistory[]>([]);
+        const messageEndRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const fetchChatHistory = async () => {
-            const data = await getChatHistory(CHAT_HISTORY_KEY);
+        useEffect(() => {
+            const fetchChatHistory = async () => {
+                const data = await getChatHistory(CHAT_HISTORY_KEY);
 
-            if (!data) return;
-            setChatHistory((prev) => [...prev, ...data]);
-        };
-
-        fetchChatHistory();
-    }, []);
-
-    /**
-     * Handling the response from AI
-     */
-    const handleAiResponse = async (): Promise<void> => {
-        const modelService = new ModelService();
-        modelService.selectModel(GEMINI_API, "gemini-2.0-flash-001");
-
-        const { error, success } = await modelService.generate({
-            prompt: prompt,
-            systemPrompt: systemPrompt,
-            extractedCode: code,
-        });
-
-        if (success) {
-            const res: ChatHistory = {
-                role: "assistant",
-                content: success,
+                if (!data) return;
+                setChatHistory((prev) => [...prev, ...data]);
             };
 
-            const updatedChatHistory = [...chatHistory, res];
-            addChatHistory(CHAT_HISTORY_KEY, updatedChatHistory);
+            fetchChatHistory();
+        }, []);
 
-            setChatHistory((prev) => [...prev, res]);
-        }
+        useEffect(() => {
+            // for scrolling to the bottom of the chat
+            if (messageEndRef.current) {
+                messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+            }
 
-        if (error) {
-            console.log(error);
-        }
-    };
+            const saveChatHistory = async () => {
+                await addChatHistory(CHAT_HISTORY_KEY, chatHistory);
+            };
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+            saveChatHistory();
+        }, [chatHistory]);
 
-        setPrompt(value);
-        const newMsg: ChatHistory = { role: "user", content: value };
-        const updatedChatHistory = [...chatHistory, newMsg];
-        addChatHistory(CHAT_HISTORY_KEY, updatedChatHistory);
+        /**
+         * Handling the response from AI
+         */
+        const handleAiResponse = async (): Promise<void> => {
+            const modelService = new ModelService();
+            modelService.selectModel(GEMINI_API, "gemini-2.0-flash-001");
 
-        setChatHistory((prev) => [...prev, newMsg]);
+            const { error, success } = await modelService.generate({
+                prompt: prompt,
+                systemPrompt: systemPrompt,
+                extractedCode: code,
+            });
 
-        handleAiResponse();
-        setValue("");
-    };
+            if (success) {
+                const res: ChatHistory = {
+                    role: "assistant",
+                    content: success,
+                };
 
-    // Function to handle Enter key press
-    function handleEnterKeyPress(e: React.KeyboardEvent<HTMLTextAreaElement>) {
-        if (e.key === "Enter" && !e.shiftKey) {
+                setChatHistory((prev) => [...prev, res]);
+            }
+
+            if (error) {
+                console.log(error);
+            }
+        };
+
+        const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
             e.preventDefault();
 
             setPrompt(value);
             const newMsg: ChatHistory = { role: "user", content: value };
-            const updatedChatHistory = [...chatHistory, newMsg];
-            addChatHistory(CHAT_HISTORY_KEY, updatedChatHistory);
 
             setChatHistory((prev) => [...prev, newMsg]);
 
             handleAiResponse();
             setValue("");
+        };
+
+        // Function to handle Enter key press
+        function handleEnterKeyPress(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+            if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault();
+
+                setPrompt(value);
+                const newMsg: ChatHistory = { role: "user", content: value };
+
+                setChatHistory((prev) => [...prev, newMsg]);
+
+                handleAiResponse();
+                setValue("");
+            }
         }
-    }
 
-    useEffect(() => {
-        if (messageEndRef.current) {
-            messageEndRef.current.scrollIntoView({ behavior: "smooth" });
-        }
-    }, [chatHistory]);
+        return (
+            <div
+                style={{
+                    width: maximize ? "600px" : "360px",
+                    height: "500px",
+                }}
+                className="content-container"
+            >
+                <nav className="content-navbar">
+                    <button
+                        onClick={() => setMaximize(!maximize)}
+                        className="content-max-btn"
+                        title={maximize ? "Minimize" : "Maximize"}
+                    >
+                        {maximize ? (
+                            <Minimize2 size={12} color="#262626" />
+                        ) : (
+                            <Maximize2 size={12} color="#262626" />
+                        )}
+                    </button>
+                </nav>
 
-    return (
-        <div
-            style={{
-                width: maximize ? "600px" : "360px",
-                height: "500px",
-            }}
-            className="content-container"
-        >
-            <nav className="content-navbar">
-                <button
-                    onClick={() => setMaximize(!maximize)}
-                    className="content-max-btn"
-                    title={maximize ? "Minimize" : "Maximize"}
-                >
-                    {maximize ? (
-                        <Minimize2 size={12} color="#262626" />
-                    ) : (
-                        <Maximize2 size={12} color="#262626" />
-                    )}
-                </button>
-            </nav>
-
-            <section className="content-chat">
-                {chatHistory &&
-                    chatHistory.map((chat, idx) => (
-                        <article key={idx}>
-                            {chat.role === "user" && typeof chat.content === "string" && (
-                                <div className="user-chat-container">
-                                    <div className="user-chat">{<div>{chat.content}</div>}</div>
-                                </div>
-                            )}
-                            {chat.role === "assistant" && typeof chat.content === "object" && (
-                                <div className="ai-response">
-                                    <div>{chat.content.feedback}</div>
-                                    <div className="hints">
-                                        <Accordion className="accordion" type="single" collapsible>
-                                            {chat.content.hints?.map((hint, idx) => (
-                                                <AccordionItem
-                                                    key={idx}
-                                                    value={`hint ${idx}`}
-                                                    className="accordion-item"
-                                                >
-                                                    <AccordionTrigger className="accordion-trigger">
-                                                        <span>Hint {idx + 1}</span>
-                                                    </AccordionTrigger>
-
-                                                    <AccordionContent className="accordion-content">
-                                                        {hint}
-                                                    </AccordionContent>
-                                                </AccordionItem>
-                                            ))}
-                                        </Accordion>
+                <section className="content-chat">
+                    {chatHistory &&
+                        chatHistory.map((chat, idx) => (
+                            <article key={idx}>
+                                {chat.role === "user" && typeof chat.content === "string" && (
+                                    <div className="user-chat-container">
+                                        <div className="user-chat">{<div>{chat.content}</div>}</div>
                                     </div>
-                                </div>
-                            )}
-                        </article>
-                    ))}
+                                )}
+                                {chat.role === "assistant" && typeof chat.content === "object" && (
+                                    <div className="ai-response">
+                                        <div>{chat.content.feedback}</div>
+                                        <div className="hints">
+                                            <Accordion
+                                                className="accordion"
+                                                type="single"
+                                                collapsible
+                                            >
+                                                {chat.content.hints?.map((hint, idx) => (
+                                                    <AccordionItem
+                                                        key={idx}
+                                                        value={`hint ${idx}`}
+                                                        className="accordion-item"
+                                                    >
+                                                        <AccordionTrigger className="accordion-trigger">
+                                                            <span>Hint {idx + 1}</span>
+                                                        </AccordionTrigger>
 
-                <div ref={messageEndRef} />
-            </section>
-            <form className="content-form" onSubmit={(e) => handleSubmit(e)}>
-                <div className="textarea-container">
-                    <Textarea
-                        value={value}
-                        onChange={(e) => setValue(e.target.value)}
-                        placeholder="Ask for a Hint"
-                        onKeyDown={handleEnterKeyPress}
-                    />
-                </div>
-                <button type="submit" className="content-send-btn">
-                    <SendHorizontal size={10} color="#f5f5f5" />
-                </button>
-            </form>
-        </div>
-    );
-}
+                                                        <AccordionContent className="accordion-content">
+                                                            {hint}
+                                                        </AccordionContent>
+                                                    </AccordionItem>
+                                                ))}
+                                            </Accordion>
+                                        </div>
+                                    </div>
+                                )}
+                            </article>
+                        ))}
+
+                    <div ref={messageEndRef} />
+                </section>
+                <form className="content-form" onSubmit={(e) => handleSubmit(e)}>
+                    <div className="textarea-container">
+                        <Textarea
+                            ref={ref}
+                            value={value}
+                            onChange={(e) => setValue(e.target.value)}
+                            placeholder="Ask for a Hint"
+                            onKeyDown={handleEnterKeyPress}
+                        />
+                    </div>
+                    <button type="submit" className="content-send-btn">
+                        <SendHorizontal size={10} color="#f5f5f5" />
+                    </button>
+                </form>
+            </div>
+        );
+    }
+);
